57c9b9c1af2942fa55b807c09ab711201493e218
vdev3094 <66820680+vdev3094@users.noreply.github.com>
2022-03-22 20:58:11 +0200

    project set up, authentication & middleware

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,6 @@ import NextAuth from "next-auth";
 import { JWT } from "next-auth/jwt";
 import SpotifyProvider from "next-auth/providers/spotify";
 import spotifyApi, { LOGIN_URL } from "../../../lib/spotify";
-import { getProviders } from "next-auth/react";
 
 const refreshAccessToken = async (token: JWT) => {
   try {
