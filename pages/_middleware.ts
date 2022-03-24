0ae5875949d41fd14feec101eb465109842ad458
vdev3094 <66820680+vdev3094@users.noreply.github.com>
2022-03-24 17:01:51 +0200

    fixed!

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,13 +1,15 @@
 import { NextApiRequest } from "next";
 import { getToken } from "next-auth/jwt";
-import { getSession } from "next-auth/react";
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(
   req: NextRequest,
   event: NextFetchEvent
 ) {
-  const token = await getSession({ req });
+  const token = await getToken({
+    req,
+    secret: process.env.JWT_SECRET!,
+  });
 
   const { pathname }: { pathname: string } = req.nextUrl;
 
