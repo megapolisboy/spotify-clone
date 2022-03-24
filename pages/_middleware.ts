147edccfa96a73cb34481b8815a8a93ae9890502
vdev3094 <66820680+vdev3094@users.noreply.github.com>
2022-03-24 17:05:52 +0200

    middleware token fixed

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,22 +2,16 @@ import { NextApiRequest } from "next";
 import { getToken } from "next-auth/jwt";
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
-export default async function middleware(
-  req: NextRequest,
-  event: NextFetchEvent
-) {
-  const token = await getToken({
-    req,
-    secret: process.env.JWT_SECRET!,
-  });
+export default async function middleware(req: NextRequest) {
+  const cookie = req.cookies["auth"];
 
   const { pathname }: { pathname: string } = req.nextUrl;
 
-  if (pathname.includes("/api/auth") || token) {
+  if (pathname.includes("/api/auth") || cookie) {
     return NextResponse.next();
   }
 
-  if (!token && pathname !== "/login") {
+  if (!cookie && pathname !== "/login") {
     return NextResponse.redirect("/login");
   }
 }
